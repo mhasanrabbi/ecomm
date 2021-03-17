151a886ea1202bd6226fd2d384f92b389847a8eb
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-03-17 13:26:36 +0600

    landing page complete

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -16,7 +16,7 @@ module.exports = ({ products }) => {
         </td>
         <td>
           <form method="POST" action="/admin/products/${product.id}/delete">
-            <button class="button is-danger" href="">Delete</button>
+            <button class="button is-danger">Delete</button>
           </form>
         </td>
       </tr>
